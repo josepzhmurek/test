54128dcf656581277256b6d294d49b054cdb7aba
josepzhmurek <josepzhmurek@gmail.com>
2018-04-26 15:54:01 +0300

    Добавьте файлы проекта.

---
+++ TestForGit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestForGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi git! Hi github!");
+            Console.ReadKey();
+        }
+    }
+}
